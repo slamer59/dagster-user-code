b1a78a8a37f4128b28831ffe5eab53148490a700
Pedot T <thomas.pedot@gmail.com>
2021-05-03 18:55:04 +0200

    Initialisation Code utilisateur

---
+++ README.md
@@ -0,0 +1,1 @@
+# dagster-user-code
---
+++ serial_pipeline.py
@@ -0,0 +1,43 @@
+import csv
+
+import requests
+from dagster import pipeline, solid
+
+
+@solid
+def download_cereals(_):
+    response = requests.get("https://docs.dagster.io/assets/cereal.csv")
+    lines = response.text.split("\n")
+    return [row for row in csv.DictReader(lines)]
+
+
+@solid
+def find_highest_calorie_cereal(_, cereals):
+    sorted_cereals = list(
+        sorted(cereals, key=lambda cereal: cereal["calories"])
+    )
+    return sorted_cereals[-1]["name"]
+
+
+@solid
+def find_highest_protein_cereal(_, cereals):
+    sorted_cereals = list(
+        sorted(cereals, key=lambda cereal: cereal["protein"])
+    )
+    return sorted_cereals[-1]["name"]
+
+
+@solid
+def display_results(context, most_calories, most_protein):
+    context.log.info(f"Most caloric cereal: {most_calories}")
+    context.log.info(f"Most protein-rich cereal: {most_protein}")
+
+
+@pipeline
+def complex_pipeline():
+    cereals = download_cereals()
+    display_results(
+        most_calories=find_highest_calorie_cereal(cereals),
+        most_protein=find_highest_protein_cereal(cereals),
+    )
+
